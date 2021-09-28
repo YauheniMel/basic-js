cbe5e92d66331720960dc5921a5778f85004dced
YauheniMel <207melnik@gmail.com>
2021-09-28 15:16:29 +0300

    feat: dream-team finished

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,18 +2,31 @@ import { NotImplementedError } from '../extensions/index.js';
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-export default function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+export default function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if(Array.isArray(members)) {
+    const dremTeamRandom = members.map((item) => {
+      if(!isFinite(item) && item && typeof item == 'string') {
+        return item.trim()[0].toUpperCase();
+      }
+    })
+
+    const dremTeam = dremTeamRandom.sort();
+
+    return dremTeam.join("");
+  }
+
+  return false;
 }
