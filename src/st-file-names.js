73537fb155f635620fa47354942fd68e8f1b3953
YauheniMel <207melnik@gmail.com>
2021-10-01 14:58:26 +0300

    feat: file-names finished

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,33 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
+export default function renameFiles(names) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let newArr = [];
+  names.forEach((item, index) => {
+    let i = 0;
+
+    for(let n = 0; n <= newArr.length; n++) {
+      if(item == newArr[n]) {
+        i++;
+        newArr.push(`${item}(${i})`);
+        return;
+      }
+    }
+    newArr.push(item);
+  })
+
+  for(let a = 0; a < newArr.length; a++) {
+    for(let b = a; b < newArr.length; b++) {
+      if(newArr[a] == newArr[b + 1]) {
+        console.log(newArr[b + 1]);
+        let num = +newArr[b + 1].match(/\d/);
+
+        newArr.splice(b + 1, 1, newArr[b + 1].replace(/\d/, num + 1))
+      }
+    }
+  }
+
+  return newArr;
 }
