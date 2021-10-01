841844eb094b3943a50ad4b93ea2ef45a7730bbf
YauheniMel <207melnik@gmail.com>
2021-10-01 17:20:48 +0300

    feat: matrix-elements finished

--- src/st-matrix-elements-sum.js
+++ src/st-matrix-elements-sum.js
@@ -16,7 +16,43 @@ import { NotImplementedError } from '../extensions/index.js';
  *
  * The result should be 9
  */
-export default function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+export default function getMatrixElementsSum(...matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  matrix = matrix[0];
+
+  if([...matrix][0] == 0) return 0;
+  let arrIdx = [];
+  let arr = [];
+  for(let i = 0; i < matrix.length; i++) {
+    for(let n = 0; n < matrix[i].length; n++) {
+      if(matrix[i][n] !== 0) arr.push(n);
+    }
+
+    if([...arr][0] == undefined) break;
+    arrIdx.push([...arr]);
+    arr = [];
+  }
+  let sum = 0;
+  let regExp = new RegExp(`[${arrIdx[0].join("")}]+`)
+
+  arr = [];
+  for(let k = 0; k < arrIdx.length; k++) {
+    if(arrIdx[k]) {
+      if(arrIdx[k].join("").match(regExp)) {
+        let res = arrIdx[k].join("").match(regExp)[0];
+        arr.push([...res.split("")]);
+        regExp = new RegExp(`[${res}]+`)
+      } else {
+        break;
+      }
+    }
+  }
+
+  arr.forEach((item, index) => {
+    item.forEach(item2 => {
+      sum += [...matrix][index][item2];
+    })
+  })
+  return sum;
 }
